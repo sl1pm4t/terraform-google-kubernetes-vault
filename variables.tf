45ea49b2f6e27b7e03f04e76a831a5a57970e161
Matt Morrison <matt@mojo.net.nz>
2018-12-12 13:02:56 +1300

    Update vault to 0.11.4

--- variables.tf
+++ variables.tf
@@ -68,7 +68,7 @@ variable vault_image_repository {
 
 variable vault_image_tag {
   description = "Docker image tag of 'vault' container"
-  default     = "0.11.1"
+  default     = "0.11.4"
 }
 
 variable vault_load_balancer_fqdn {
